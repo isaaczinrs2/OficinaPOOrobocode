7b91a4a316335b58af986c258057350c97449f21
Isaac Amorim <163526904+isaaczinrs2@users.noreply.github.com>
2025-05-13 17:07:19 -0300

    Quarta Atividade

---
+++ Basic Math Operations.java
@@ -0,0 +1,20 @@
+public class BasicOperations
+{
+  public static Integer basicMath(String op, int v1, int v2)
+  {
+    return 0;
+  }
+  
+  class calculadora {
+    public static void main(String[] args){
+      
+    
+    int v1 = 4;
+    int v2 = 5;
+   
+    int soma = v1 + v2;
+    System.out.println("O resultado da soma é:" + soma);
+    }
+    
+  }
+}
